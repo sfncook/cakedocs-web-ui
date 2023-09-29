a5990a2829f646d3b0721430d215fd5a2e15d232
Shawn Cook <sfncook@gmail.com>
2023-09-28 17:03:20 -0700

    Add repos

--- pages/api/repos.js
+++ pages/api/repos.js
@@ -1,6 +1,8 @@
 
 export default function handler(req, res) {
   res.status(200).json({ repos: [
-      { repo_name: 'sfncook/gpt-interview-coach', repo_url: 'https://github.com/sfncook/gpt-interview-coach'}
+      { repo_name: 'sfncook/gpt-interview-coach', repo_url: 'https://github.com/sfncook/gpt-interview-coach'},
+      { repo_name: 'sfncook/hello', repo_url: 'https://github.com/sfncook/hello'},
+      { repo_name: 'sfncook/spring-petclinic', repo_url: 'https://github.com/sfncook/spring-petclinic'},
     ] })
 }
