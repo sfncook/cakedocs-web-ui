4dafc9bf8dde47ffa6ca2f1533f95f2955ac6897
Shawn Cook <sfncook@gmail.com>
2023-09-28 18:13:18 -0700

    Add SoulGroups

--- pages/api/repos.js
+++ pages/api/repos.js
@@ -4,5 +4,6 @@ export default function handler(req, res) {
       { repo_name: 'sfncook/gpt-interview-coach', repo_url: 'https://github.com/sfncook/gpt-interview-coach'},
       { repo_name: 'sfncook/hello', repo_url: 'https://github.com/sfncook/hello'},
       { repo_name: 'sfncook/spring-petclinic', repo_url: 'https://github.com/sfncook/spring-petclinic'},
+      { repo_name: 'bryantjk/SoulGroups', repo_url: 'https://github.com/bryantjk/SoulGroups'},
     ] })
 }
